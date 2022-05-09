d71738a328fad92c72d12d2da93af6f3799375ea
Valeria <lerapetrovic8@gmail.com>
2022-05-09 17:20:23 +0300

    feat:code

--- script.js
+++ script.js
@@ -4,12 +4,6 @@ const keyboard = [
     "a", "s", "d", "f", "g", "h", "j", "k", "l",";","'","Enter","Shift","/",
     "z","x", "c", "v", "b", "n", "m", ",",".","/","↑","Shft","Ctrl","Win","Alt"," ","Alt","Ctrl","←","↓","→"
 ];
-const keyboardANGL = [
-    "ё","1", "2", "3", "4", "5", "6", "7", "8", "9", "0","-","=", "Backspace",
-    "Tab","й", "ц", "у", "к", "е","н",  "г", "ш", "щ", "з", "[","]","/","Del","CapsLk",
-    "ф", "ы", "в", "а", "п", "р", "о", "л", "д",";","'","Enter","Shift","/",
-    "я","ч", "с", "м", "и", "т", "ь", "б","ю","/","↑","Shft","Ctrl","Win","Alt"," ","Alt","Ctrl","←","↓","→"
-];
 // заполнение клавиатуры
 function init(){
     let out = '';
@@ -85,21 +79,44 @@ document.getElementById('windowScreen').value=valu.slice(0,z);
 var capslock = false;
 //Capslock
 function funcCL(){
-    document.querySelector("[data='CapsLk']").classList.add('activCL');
-    (function () {
-        for (var btns = document.getElementById ('keyboard').getElementsByTagName ('div'),
-    j = 0, J = btns.length; j < J; j++) with (btns [j])
-  if(j<13||j>15){
-      if(j<29||j>31){
-          if(j<43||j>45){
-              if(j<58||j>62){
-                  if(j<64||j>65){
-    if (!id)
-  onclick = function () {document.getElementById('windowScreen').value += this.innerHTML.toUpperCase();
-  this.classList.add('active');
+    var color  = ['mediumslateblue','darkgrey'];
+    if (document.querySelector("[data='CapsLk']").style.background != color[0]){
+        document.querySelector("[data='CapsLk']").style.background = color[0];
+        (function () {
+            for (var btns = document.getElementById ('keyboard').getElementsByTagName ('div'),
+        j = 0, J = btns.length; j < J; j++) with (btns [j])
+      if(j<13||j>15){
+          if(j<29||j>31){
+              if(j<43||j>45){
+                  if(j<58||j>62){
+                      if(j<64||j>65){
+        if (!id)
+      onclick = function () {document.getElementById('windowScreen').value += this.innerHTML.toUpperCase();
+      this.classList.add('active');
+      
+    }
+      }}}}}}) ();
+    }
+    else{
+        document.querySelector("[data='CapsLk']").style.background = color[1];
+        (function () {
+            for (var btns = document.getElementById ('keyboard').getElementsByTagName ('div'),
+        j = 0, J = btns.length; j < J; j++) with (btns [j])
+      if(j<13||j>15){
+          if(j<29||j>31){
+              if(j<43||j>45){
+                  if(j<58||j>62){
+                      if(j<64||j>65){
+        if (!id)
+      onclick = function () {document.getElementById('windowScreen').value += this.innerHTML;
+      this.classList.add('active');
+      
+    }
+      }}}}}}) ();
+    }
+
+   
   
-}
-  }}}}}}) ();
  
     
 } //при повторном нажатии не работает 
@@ -114,17 +131,6 @@ function funcClear(){
 
 //Shift
 function funcShift(){
-
-  
-   /*
-    var valu=document.getElementById('windowScreen').value;
-    document.getElementById('windowScreen').value=valu.toUpperCase();
-    /*if(Fshift=='0'){
-        Fshift='1';
-    }
-    else if (Fshift=='1'){
-        Fshift='0';
-    }*/
 }
 
 
@@ -169,23 +175,32 @@ Shift.addEventListener('click',funcShift);
 
 
 //сочетание клавишь
-
+var ispressbtn=false;
 let flag = false;
 document.onkeydown=function(event){
-    console.log(event);
-    if(event.code='Shift') flag=true;
+    
+    if(event.code='Ctrl') flag=true;
     if(event.code='AltLeft'&& flag)
-    { flag=false; //даёт возможность при удерживании срабатывать только один раз
+    { flag=false;
+         //даёт возможность при удерживании срабатывать только один раз
+     if(!ispressbtn){
+    const keyboard = [
+        "ё","1", "2", "3", "4", "5", "6", "7", "8", "9", "0","-","=", "Backspace",
+    "Tab","й", "ц", "у", "к", "е","н",  "г", "ш", "щ", "з", "[","]","/","Del","CapsLk",
+    "ф", "ы", "в", "а", "п", "р", "о", "л", "д",";","'","Enter","Shift","/",
+    "я","ч", "с", "м", "и", "т", "ь", "б","ю","/","↑","Shft","Ctrl","Win","Alt"," ","Alt","Ctrl","←","↓","→"
+    ];
     function init(){
         let out = '';
-        for(let i = 0; i < keyboardANGL.length; i++){
+        for(let i = 0; i < keyboard.length; i++){
             if(i == 14 || i == 29 || i == 42|| i == 56){
                 out +='<div class="clearfix"></div>';  //перенос
             }
            
-            out +='<div class="k-key" data="' + keyboardANGL[i] + '" >'+keyboardANGL[i]+'</div>'; // создание элементов div
+            out +='<div class="k-key" data="' + keyboard[i] + '" >'+keyboard[i]+'</div>'; // создание элементов div
         }
         document.querySelector('#keyboard').innerHTML = out;
+    
     
     }           
     init();
@@ -197,29 +212,64 @@ document.onkeydown=function(event){
         });
         document.querySelector('#keyboard .k-key[data="' +event.keyCode + '"]').classList.add('active');
         
-    } 
-//вывод на экран
-(function () {
-    for (var btns = document.getElementById ('keyboard').getElementsByTagName ('div'),
-j = 0, J = btns.length; j < J; j++) with (btns [j])
-if(j<13||j>15){
-  if(j<29||j>31){
-      if(j<43||j>45){
-          if(j<58||j>62){
-              if(j<64||j>65){
-if (!id)
-onclick = function () {document.getElementById('windowScreen').value += this.innerHTML;
-this.classList.add('active');}
-}}}}}}) ();
+    }
+    document.querySelectorAll('#keyboard .k-key').forEach(function (element) {
+    element.onclick = function(event){
+        document.querySelectorAll('#keyboard .k-key').forEach(function (element) {
+            element.classList.remove('active');
+        });
+        let code  = this.getAttribute('data');
+        this.classList.add('active');
+        console.log(code);
+    }
+    });
+    
+      //вывод на элемент texteria
+    
+      (function () {
+          for (var btns = document.getElementById ('keyboard').getElementsByTagName ('div'),
+      j = 0, J = btns.length; j < J; j++) with (btns [j])
+    if(j<13||j>15){
+        if(j<29||j>31){
+            if(j<43||j>45){
+                if(j<58||j>62){
+                    if(j<64||j>65){
+      if (!id)
+    onclick = function () {document.getElementById('windowScreen').value += this.innerHTML;
+    this.classList.add('active');}
+    }}}}}}) ();
+    
+    document.addEventListener ('keydown', function (event){
+      console.log (event);
+    }); 
+    var evt = new KeyboardEvent('keydown', {'keyCode':65, 'which':65});
+    document.dispatchEvent(evt);
+    
+    const del = document.querySelector("[data='Del']");
+del.addEventListener('click',funcClear);
+const Backspace = document.querySelector("[data='Backspace']");
+Backspace.addEventListener('click',funcDel);
+const CapsLk = document.querySelector("[data='CapsLk']");
+CapsLk.addEventListener('click',funcCL);
 
-document.addEventListener ('keydown', function (event){
-console.log (event);
-}); 
-var evt = new KeyboardEvent('keydown', {'keyCode':65, 'which':65});
-document.dispatchEvent(evt);
 
+const Enter = document.querySelector("[data='Enter']");
+Enter.addEventListener('click',funcEnter);
+const Tab = document.querySelector("[data='Tab']");
+Tab.addEventListener('click',funcTab);
+const Shift = document.querySelector("[data='Shift']");
+Shift.addEventListener('click',funcShift);
 
+}}
+else{
+    ispressbtn=false;
+    
 }
+
+
+
+
+
 }
 //работает построение клавы, но не работает вывод элементов и обратное переключение 
 
